3b68c25cd6cb93203025f791eec060b69c0aaf0f
linggeli7 <linggeli@dhcp-v211-221.vp.reshsg.uci.edu>
2017-03-25 14:44:43 -0700

    new path

--- path/path.R
+++ path/path.R
@@ -1,8 +1,8 @@
 library(ggplot2)
 
-path <- rbind(cbind(bootstrap$V1[1:20], bootstrap$V2[1:20]), 
-              cbind(gaussian$V1[1:20], gaussian$V2[1:20]), 
-              cbind(hadamard$V1[1:20], hadamard$V2[1:20]))
+path <- rbind(cbind(bootstrap$V1, bootstrap$V2), 
+              cbind(gaussian$V1, gaussian$V2), 
+              cbind(hadamard$V1, hadamard$V2))
 
 path <- as.data.frame(path)
 
@@ -10,24 +10,21 @@ path$method <- c(rep('subsampling', 20), rep('gaussian', 20), rep('hadamard', 20
 
 colnames(path)[1:2] <- c('beta1', 'beta2')
 
-dataY <- as.matrix(dataY)
-dataX <- as.matrix(dataX)
-summary(lm(dataY ~ 0 + dataX))
-
 dens <- matrix(NA, nrow=400, ncol=3)
 for (i in 1:20) {
   for (j in 1:20) {
-    dens[i + 20 * (j - 1), 1] <- 0.4 + 0.05 * i
-    dens[i + 20 * (j - 1), 2] <- -2.4 + 0.05 * j
-    dens[i + 20 * (j - 1), 3] <- dnorm(0.4 + 0.05 * i, 0.94, 0.1) * dnorm(-2.4 + 0.05 * j, -1.8, 0.1)
+    dens[i + 20 * (j - 1), 1] <- -1.4 + 0.02 * i
+    dens[i + 20 * (j - 1), 2] <- -1.9 + 0.05 * j
+    dens[i + 20 * (j - 1), 3] <- dnorm(-1.4 + 0.02 * i, -1.2, 0.1) * dnorm(-1.9 + 0.05 * j, -1.26, 0.1)
   }
 }
 dens <- as.data.frame(dens)
 colnames(dens) <- c('x', 'y', 'p')
 
 ggplot(data=path) +
-  geom_path(aes(x=beta1, y=beta2, colour=method, linetype=method)) +
-  geom_contour(data=dens, aes(x=x, y=y, z=p), colour='black')
+  geom_path(aes(x=beta1, y=beta2, colour=method)) +
+  geom_contour(data=dens, aes(x=x, y=y, z=p), colour='darkgray', bins=4) +
+  theme_bw()
 
 S <- c(14.56, 6.26, 3.03)
 G <- c(13.07, 5.92, 3.16)
@@ -43,5 +40,6 @@ ratio$method <- c(rep('subsampling', 3), rep('gaussian', 3), rep('hadamard', 3))
 
 ggplot(data=ratio) +
   geom_point(aes(x=r, y=ratio, colour=method)) +
-  geom_path(aes(x=r, y=ratio, colour=method))
+  geom_path(aes(x=r, y=ratio, colour=method)) +
+  theme_bw()
 
